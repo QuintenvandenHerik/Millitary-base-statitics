6da2c592cc8106c94113d44e5314fa4459cc30cd
Quinten van den Herik <99041030@mydavinci.nl>
2019-04-08 11:10:59 +0200

    Millitary basic statistics Laravel

---
+++ database/migrations/2019_04_02_100052_create_tests_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('soldier_name');
+            $table->string('soldier_lastname');
+            $table->string('soldier_type');
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tests');
+    }
+}
---
+++ database/seeds/TestSeeder.php
@@ -0,0 +1,23 @@
+<?php
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class TestSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+    $faker = Faker\Factory::create();
+
+    for($i = 0; $i < 1000; $i++) {
+        App\Test::create([
+            'soldier_name' => $faker->firstname,
+            'soldier_lastname' => $faker->lastname,
+            'soldier_type' => $faker->bothify,
+        ]);
+    }
+}
+}
